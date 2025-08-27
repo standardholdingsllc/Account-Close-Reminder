f73a20452a0b0c3afbe524a6f59c47ae1796785e
jonah larson <contact@standardholdingsllc.com>
2025-08-27 16:21:38 -0500

    Update

--- next.config.js
+++ next.config.js
@@ -2,29 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  
-  // Vercel serverless function configuration
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-  },
-
-  // Optimize for serverless
-  target: 'serverless',
-
-  // API routes configuration
-  async rewrites() {
-    return [
-      // No rewrites needed for this project
-    ];
-  },
-
-  // Environment variables that should be exposed to the browser
-  env: {
-    // Don't expose sensitive environment variables here
-  }
 };
 
 module.exports = nextConfig;
